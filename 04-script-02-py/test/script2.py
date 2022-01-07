7dba7281c1f6bc16cdef1417b6c0c8ef8767494c
Evgeniy Sulimov <evgeny.sulimov@gmail.com>
2022-01-07 12:39:54 +0300

    Exercise 02

--- 04-script-02-py/test/script2.py
+++ 04-script-02-py/test/script2.py
@@ -4,6 +4,8 @@ import os
 
 bash_command = ["cd ~/netology/sysadm-homeworks", "git status"]
 result_os = os.popen(' && '.join(bash_command)).read()
+# Получаем полный путь рабочей директории (директория локального репозитория)
+wd_path = os.path.expanduser("~/netology/sysadm-homeworks")
 # Не используемая переменная
 # is_change = False
 for result in result_os.split('\n'):
@@ -11,6 +13,6 @@ for result in result_os.split('\n'):
         prepare_result = result.replace('\tизменено:   ', '')
 #       print(prepare_result)
 # Печатаем полный путь к файлу
-        print(f"{os.getcwd()}/{prepare_result}".replace(" ", ""))
+        print(f"{wd_path}/{prepare_result}".replace(" ", ""))
 # Отключаем преждевременный выход
 #        break
