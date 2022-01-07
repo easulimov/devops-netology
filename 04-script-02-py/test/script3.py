12997bb7f4f3c577beeaf9c9d667f6a80d1ce6dd
Evgeniy Sulimov <evgeny.sulimov@gmail.com>
2022-01-07 18:08:26 +0300

    Exercise 03

--- 04-script-02-py/test/script3.py
+++ 04-script-02-py/test/script3.py
@@ -31,7 +31,8 @@ def find_modified_files(dir_path):
         if result.find('изменено') != -1:
             status += 1
             prepare_result = result.replace('\tизменено:   ', '')
-            print(f"{dir_path}/{prepare_result}".replace(" ", ""))
+            final_result = (f"{dir_path}/{prepare_result}".replace(" ", "")).replace("//", "/")
+            print(final_result)
     if (status == 0):
         print(f"В репозитории по указанному пути {dir_path} - нет измененных файлов")
 
